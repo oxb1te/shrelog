bb500b93be620d77c990fca68f0745e4098d4a5e
icoder-new <priler05@gmail.com>
2023-05-11 16:13:04 +0500

    more changes

--- shrelog.sh
+++ shrelog.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-if ! command -v shred > /dev/null; then
-  echo "Error: shred not found. Please install it and try again."
-  exit 1
-fi
+check_command() {
+  command_name=$1
+  if ! command -v "$command_name" >/dev/null; then
+    echo "Error: $command_name not found. Please install it and try again."
+    exit 1
+  fi
+}
 
-files_to_delete=(
-  ~/.bash_history
-  ~/.zsh_history
-  ~/.viminfo
-  ~/.mysql_history
-  ~/.python_history
-  ~/.psql_history
-  ~/.nano_history
-  ~/.node_repl_history
-  ~/.wget-hsts
-)
+delete_files() {
+  files=("$@")
+  for file in "${files[@]}"; do
+    if [ -f "$file" ]; then
+      shred -v -n 3 "$file" || {
+        echo "Error: Failed to delete file: $file"
+        exit 1
+      }
+      echo "Deleted file: $file"
+    fi
+  done
+}
 
-dirs_to_delete=(
-  ~/.cache/chromium
-  ~/.cache/google-chrome
-  ~/.mozilla/firefox
-)
-
-sensitive_logs=(
-  "/var/log/auth.log"
-  "/var/log/secure"
-  "/var/log/syslog"
-  "/var/log/nginx/access.log"
-  "/var/log/nginx/error.log"
-  "/var/log/httpd/access_log"
-  "/var/log/httpd/error_log"
-)
+delete_directories() {
+  dirs=("$@")
+  for dir in "${dirs[@]}"; do
+    if [ -d "$dir" ]; then
+      find "$dir" -type f -exec shred -n 3 -f -z {} + || {
+        echo "Error: Failed to delete directory: $dir"
+        exit 1
+      }
+      echo "Deleted directory: $dir"
+    fi
+  done
+}
 
 read_sensitive_strings() {
   local sensitive_log=$1
@@ -46,6 +47,7 @@ read_sensitive_strings() {
     echo "Error: log file not found: $sensitive_log"
   fi
 }
+
 echo 'Script created by @r3x08.'
 echo "Select an option:"
 echo "1. Delete log files"
@@ -56,25 +58,42 @@ read -p "Enter your choice (1-4): " choice
 
 case "$choice" in
   1)
-    # Delete files
-    for file in "${files_to_delete[@]}"; do
-      if [ -f "$file" ]; then
-        shred -v -n 3 "$file"
-        echo "Deleted file: $file"
-      fi
-    done
-
-    # Delete directories
-    for dir in "${dirs_to_delete[@]}"; do
-      if [ -d "$dir" ]; then
-        find "$dir" -type f -exec shred -n 3 -f -z {} +
-        echo "Deleted directory: $dir"
-      fi
-    done
+    check_command "shred"
+    files_to_delete=(
+      ~/.bash_history
+      ~/.zsh_history
+      ~/.viminfo
+      ~/.mysql_history
+      ~/.python_history
+      ~/.psql_history
+      ~/.nano_history
+      ~/.node_repl_history
+      ~/.wget-hsts
+    )
+    dirs_to_delete=(
+      ~/.cache/chromium
+      ~/.cache/google-chrome
+      ~/.mozilla/firefox
+    )
+    delete_files "${files_to_delete[@]}"
+    delete_directories "${dirs_to_delete[@]}"
     ;;
   2)
+    sensitive_logs=(
+      "/var/log/auth.log"
+      "/var/log/secure"
+      "/var/log/syslog"
+      "/var/log/nginx/access.log"
+      "/var/log/nginx/error.log"
+      "/var/log/httpd/access_log"
+      "/var/log/httpd/error_log"
+    )
     for sensitive_log in "${sensitive_logs[@]}"; do
-      read_sensitive_strings "$sensitive_log"
+      if [ -f "$sensitive_log" ]; then
+        read_sensitive_strings "$sensitive_log"
+      else
+        echo "Error: log file not found: $sensitive_log"
+      fi
     done
     ;;
   3)
@@ -89,4 +108,3 @@ case "$choice" in
 esac
 
 sleep 3 && clear && echo "Done."
-
